c76e3706cb65c73b3efffffc877deb3efb13cef4
frddyy <m.ferdyfauzan@gmail.com>
2023-09-26 08:47:16 +0700

    education update

--- backend/controllers/EducationController.js
+++ backend/controllers/EducationController.js
@@ -1,36 +1,116 @@
 import Education from "../models/EducationModel.js";
 import User from "../models/UserModel.js";
 
-export const getEducation = async(req, res) => {
-    try {
-        const response = await Education.findAll()
-        res.status(200).json(response)
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+// Get all education records
+export const getEducation = async (req, res) => {
+  try {
+    const response = await Education.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
+// Get education records by user_id
 export const getEducationByUserId = async (req, res) => {
   try {
-    const userId = req.params.id; // Assuming you're getting the user ID from the request parameters
-
-    // Find the user by their ID
-    const user = await User.findByPk(userId);
-
-    if (!user) {
-      return res.status(404).json({ message: 'Education not found' });
-    }
-
-    // Find education records associated with the user using the foreign key (user_id)
-    const educationRecords = await Education.findAll({
+    const userId = req.params.user_id; // Assuming the user_id is provided as a URL parameter
+    const educationData = await Education.findAll({
       where: {
         user_id: userId,
       },
     });
 
-    res.status(200).json(educationRecords);
+    res.status(200).json(educationData);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Create a new education record
+export const createEducation = async (req, res) => {
+  try {
+    const createdEducation = await Education.create(req.body);
+    res.status(200).json({ msg: "Education created" });
+    res.status(201).json(createdEducation);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Update an education record by education_id and user_id
+export const updateEducation = async (req, res) => {
+  try {
+    const userId = req.params.user_id; // Assuming the user_id is provided as a URL parameter
+    const educationId = req.params.education_id; // Assuming the education_id is provided as a URL parameter
+
+    const [updatedRowCount] = await Education.update(req.body, {
+      where: {
+        id: educationId,
+        user_id: userId,
+      },
+    });
+
+    if (updatedRowCount === 0) {
+      // No matching education entry found
+      return res.status(404).json({ error: "Education entry not found" });
+    }
+
+    res.status(200).json({ msg: "Education entry updated" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Delete an education record by education_id and user_id
+export const deleteEducation = async (req, res) => {
+  try {
+    const userId = req.params.user_id; // Assuming the user_id is provided as a URL parameter
+    const educationId = req.params.education_id; // Assuming the education_id is provided as a URL parameter
+
+    const deletedRowCount = await Education.destroy({
+      where: {
+        id: educationId,
+        user_id: userId,
+      },
+    });
+
+    if (deletedRowCount === 0) {
+      // No matching education entry found
+      return res.status(404).json({ error: "Education entry not found" });
+    }
+
+    res.status(200).json({ msg: "Education entry deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Get education by education_id and user_id
+export const getEducationByEducationIdAndUserId = async (req, res) => {
+  try {
+    const userId = req.params.user_id;
+    const educationId = req.params.education_id;
+
+    const educationData = await Education.findOne({
+      where: {
+        id: educationId,
+        user_id: userId,
+      },
+    });
+
+    if (!educationData) {
+      return res.status(404).json({ error: "Education entry not found" });
+    }
+
+    res.status(200).json(educationData);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/routes/EducationRoute.js
+++ backend/routes/EducationRoute.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { 
     getEducation,
-    getEducationByUserId
+    getEducationByUserId,
+    createEducation,
+    updateEducation,
+    deleteEducation,
  } from "../controllers/EducationController.js";
 
 const router = express.Router();
@@ -9,7 +12,17 @@ const router = express.Router();
 // Route to get all education records
 router.get('/educations', getEducation);
 
-// Route to get education records by user ID
-router.get('/users/:id/educations', getEducationByUserId);
+// Route to get education records by user_id
+router.get('/users/:user_id/educations', getEducationByUserId);
+
+// Route to create a new education record for a user
+router.post('/users/:user_id/educations', createEducation);
+
+// Route to update an education record by education_id and user_id
+router.patch('/users/:user_id/educations/:education_id', updateEducation);
+
+// Route to delete an education record by education_id and user_id
+router.delete('/users/:user_id/educations/:education_id', deleteEducation);
+
 
 export default router;
