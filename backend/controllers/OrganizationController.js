2e9e15bcc0ada4287d2c731cea934aa4bf32aa3c
Syifa Khairina <syifa.khairina.tif421@polban.ac.id>
2023-10-03 09:05:25 +0700

    crud Organizations v2

--- backend/controllers/OrganizationController.js
+++ backend/controllers/OrganizationController.js
@@ -1,49 +1,115 @@
 import Organization from "../models/OrganizationModel.js";
 
-export const getOrganization = async(req, res) => {
-    try {
-        const response = await Organization.findAll()
-        res.status(200).json(response)
-    } catch (error) {
-        console.log(error.message)
-    }
-}
-
-
-export const getOrganizationById = async(req, res) => {
+// Get all organization records
+export const getOrganization = async (req, res) => {
   try {
-      const response = await Organization.findOne({
-          where: {
-              id: req.params.id
-          }
-      })
-      res.status(200).json(response)
+    const response = await Organization.findAll();
+    res.status(200).json(response);
   } catch (error) {
-      console.log(error.message)
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
-}
+};
+
 
 export const getOrganizationByIdentityId = async (req, res) => {
   try {
-    const identityId = req.params.id; // Assuming you're getting the user ID from the request parameters
-
-    // Find the user by their ID
-    const identity = await Organization.findByPk(identityId);
-
-    if (!identity) {
-      return res.status(404).json({ message: 'Organization not found' });
-    }
-
-    // Find education records associated with the user using the foreign key (user_id)
-    const organizationRecords = await Organization.findAll({
+    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
+    const organizationData = await Organization.findAll({
       where: {
+        identityId: identityId
+      },
+    });
+
+    res.status(200).json(organizationData);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Get organization by organization_id and identity_id
+export const getOrganizationByOrganizationIdAndIdentityId = async (req, res) => {
+  try {
+    const identityId = req.params.identityId;
+    const organizationId = req.params.organizationId;
+
+    const organizationData = await  Organization.findOne({
+      where: {
+        id: organizationId,
         identityId: identityId,
       },
     });
 
-    res.status(200).json(organizationRecords);
+    if (!organizationData) {
+      return res.status(404).json({ error: "Organization entry not found" });
+    }
+
+    res.status(200).json(organizationData);
   } catch (error) {
     console.error(error.message);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Create a new organization record
+export const createOrganization = async (req, res) => {
+  try {
+    const createdOrganization = await Organization.create(req.body);
+    res.status(200).json({ msg: "Organization created" });
+    res.status(201).json(createdOrganization);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Update an organization record by id
+export const updateOrganization = async (req, res) => {
+  try {
+    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
+    const organizationId = req.params.organizationId; // Assuming the organization_id is provided as a URL parameter
+
+    const [updatedRowCount] = await Organization.update(req.body, {
+      where: {
+        id: organizationId,
+        identityId: identityId,
+      },
+    });
+
+    if (updatedRowCount === 0) {
+      // No matching organization entry found
+      return res.status(404).json({ error: "Organization entry not found" });
+    }
+
+    res.status(200).json({ msg: "Organization entry updated" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Delete an organization record by id
+export const deleteOrganization = async (req, res) => {
+  try {
+    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
+    const organizationId = req.params.organizationId; // Assuming the organization_id is provided as a URL parameter
+
+    const deletedRowCount = await Organization.destroy({
+      where: {
+        id: organizationId,
+        identityId: identityId,
+      },
+    });
+
+    if (deletedRowCount === 0) {
+      // No matching organization entry found
+      return res.status(404).json({ error: "Organization entry not found" });
+    }
+
+    res.status(200).json({ msg: "Organization entry deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
--- backend/models/OrganizationModel.js
+++ backend/models/OrganizationModel.js
@@ -7,7 +7,7 @@ const { DataTypes } = Sequelize;
 const Organization = db.define(
   "organizations",
   {
-    name: {
+    name_org: {               //change name from name to name_org
       type: DataTypes.STRING,
       allowNull: true,
     },
@@ -23,8 +23,8 @@ const Organization = db.define(
       type: DataTypes.INTEGER,
       allowNull: true,
     },
-    role: {
-      type: DataTypes.INTEGER,
+    role: {                   
+      type: DataTypes.TEXT, //change type into text
       allowNull: true,
     },
     jobdesc: {
