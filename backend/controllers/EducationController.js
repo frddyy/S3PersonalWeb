46d2b933449ab77164b714b49a4861c2310e1fbf
egisatriadyw <egi.satria22@gmail.com>
2023-10-06 00:55:09 +0700

    Backend

--- backend/controllers/EducationController.js
+++ backend/controllers/EducationController.js
@@ -1,115 +1,160 @@
 import Education from "../models/EducationModel.js";
+import path from "path";
+import { Op } from "sequelize";
+import multer from "multer";
+import Identity from "../models/IdentityModel.js";
+
+// Configure multer to specify where to store uploaded files and their names.
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "src/img/education"); // Define the destination folder
+    },
+    filename: (req, file, callback) => {
+        // Define the filename for the uploaded file
+        const extensionName = path.extname(file.originalname);
+        const filename = "education_" + req.body.name_sch + extensionName;
+        callback(null, filename);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 // Get all education records
 export const getEducation = async (req, res) => {
-  try {
-    const response = await Education.findAll();
-    res.status(200).json(response);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        let response = await Education.findAll({
+            where: {
+                identityId: req.params.identityId,
+            },
+            include: [
+                {
+                    model: Identity,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 // Get education records by identity_id
 export const getEducationById = async (req, res) => {
-  try {
-    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
-    const educationData = await Education.findAll({
-      where: {
-        identityId: identityId,
-      },
-    });
+    try {
+        const education = await Education.findOne({
+            where: {
+                id: req.params.educationId,
+            },
+        });
 
-    res.status(200).json(educationData);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
+        if (!education) return res.status(404).json({ msg: "data tidak ditemukan" });
 
-// Get education by education_id and user_id
-export const getEducationByEducationIdAndIdentityId = async (req, res) => {
-  try {
-    const identityId = req.params.identityId;
-    const educationId = req.params.educationId;
-
-    const educationData = await Education.findOne({
-      where: {
-        id: educationId,
-        identityId: identityId,
-      },
-    });
-
-    if (!educationData) {
-      return res.status(404).json({ error: "Education entry not found" });
+        let response;
+        response = await education.findOne({
+            where: {
+                [Op.and]: [{ id: education.id }, { identityId: req.params.identityId }],
+            },
+            include: [
+                {
+                    model: Identity,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    res.status(200).json(educationData);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
 };
 
 // Create a new education record
 export const createEducation = async (req, res) => {
-  try {
-    const createdEducation = await Education.create(req.body);
-    res.status(200).json({ msg: "Education created" });
-    res.status(201).json(createdEducation);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
+    try {
+        upload.single("image")(req, res, async (err) => {
+            if (err) {
+                return res.status(500).send({
+                    message: "file cannot upload",
+                });
+            }
+
+            const file_name = req.file.filename;
+            const { name_sch, start_year, end_year, major, information } = req.body;
+            await Education.create({
+                name_sch: name_sch,
+                image: file_name,
+                start_year: start_year,
+                end_year: end_year,
+                major: major,
+                information: information,
+                identityId: req.params.identityId,
+            });
+            res.status(201).json({ msg: "Education created" });
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
 // Update an education record by education_id and identity_id
 export const updateEducation = async (req, res) => {
-  try {
-    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
-    const educationId = req.params.educationId; // Assuming the education_id is provided as a URL parameter
+    try {
+        const education = await Education.findOne({
+            where: {
+                id: req.params.educationId,
+            },
+        });
 
-    const [updatedRowCount] = await Education.update(req.body, {
-      where: {
-        id: educationId,
-        identityId: identityId,
-      },
-    });
+        if (!education) return res.status(404).json({ msg: "data tidak ditemukan" });
 
-    if (updatedRowCount === 0) {
-      // No matching education entry found
-      return res.status(404).json({ error: "Education entry not found" });
+        const { name_sch, image, start_year, end_year, major, information } = req.body;
+
+        if (req.params.identityId == education.identityId) {
+            await Education.update(
+                { name_sch, image, start_year, end_year, major, information },
+                {
+                    where: {
+                        [Op.and]: [{ id: education.id }, { identityId: req.params.identityId }],
+                    },
+                }
+            );
+        } else {
+            return res.status(403).json({ msg: "akses terlarang" });
+        }
+
+        res.status(200).json({ msg: "Education entry updated" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    res.status(200).json({ msg: "Education entry updated" });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
 };
 
 // Delete an education record by education_id and identity_id
 export const deleteEducation = async (req, res) => {
-  try {
-    const identityId = req.params.identityId; // Assuming the user_id is provided as a URL parameter
-    const educationId = req.params.educationId; // Assuming the education_id is provided as a URL parameter
+    try {
+        const education = await Education.findOne({
+            where: {
+                id: req.params.educationId,
+            },
+        });
 
-    const deletedRowCount = await Education.destroy({
-      where: {
-        id: educationId,
-        identityId: identityId,
-      },
-    });
+        if (!education) return res.status(404).json({ msg: "Data tidak ditemukan" });
 
-    if (deletedRowCount === 0) {
-      // No matching education entry found
-      return res.status(404).json({ error: "Education entry not found" });
+        if (req.params.identityId == education.educationId) {
+            await Education.destroy({
+                where: {
+                    [Op.and]: [{ id: education.id }, { identityId: req.params.identityId }],
+                },
+            });
+        } else {
+            return res.status(403).json({ msg: "Akses terlarang" });
+        }
+
+        res.status(200).json({ msg: "Education entry deleted" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Internal server error" });
     }
-
-    res.status(200).json({ msg: "Education entry deleted" });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
 };
--- backend/controllers/OrganizationController.js
+++ backend/controllers/OrganizationController.js
@@ -1,144 +1,153 @@
 import Organization from "../models/OrganizationModel.js";
-import path from 'path';
+import path from "path";
 import { Op } from "sequelize";
-import multer from 'multer';
+import multer from "multer";
 import Identity from "../models/IdentityModel.js";
 
 // Configure multer to specify where to store uploaded files and their names.
 const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'src/img/organization'); // Define the destination folder
-  },
-  filename: (req, file, callback) => {
-    // Define the filename for the uploaded file
-    const extensionName = path.extname(file.originalname);
-    const filename = "organization_"+req.body.name_org + extensionName;
-    callback(null, filename);
-  },
+    destination: (req, file, callback) => {
+        callback(null, "src/image/organization"); // Define the destination folder
+    },
+    filename: (req, file, callback) => {
+        // Define the filename for the uploaded file
+        const extensionName = path.extname(file.originalname);
+        const filename = "organization_" + req.body.name_org + extensionName;
+        callback(null, filename);
+    },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 export const getOrganization = async (req, res) => {
-  try {
-    let response = await Organization.findAll({
-        where: {
-          identityId: req.params.identityId
-        },
-        include: [{
-          model: Identity,
-          attributes: ['name']
-        }]
-      });
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error)
-  }
-}
+    try {
+        let response = await Organization.findAll({
+            where: {
+                identityId: req.params.identityId,
+            },
+            include: [
+                {
+                    model: Identity,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export const getOrganizationById = async (req, res) => {
-  try {
-    const organization = await Organization.findOne({
-      where: {
-        id: req.params.organizationId
-      }
-    });
+    try {
+        const organization = await Organization.findOne({
+            where: {
+                id: req.params.organizationId,
+            },
+        });
 
-    if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
+        if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
 
-    let response;
-    response = await Organization.findOne({
-      where: {
-        [Op.and] : [{id: organization.id}, {identityId: req.params.identityId }]
-      },
-      include: [{
-        model: Identity,
-        attributes: ['name']
-      }]
-    });
-    res.status(200).json(response); 
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-}
+        let response;
+        response = await Organization.findOne({
+            where: {
+                [Op.and]: [{ id: organization.id }, { identityId: req.params.identityId }],
+            },
+            include: [
+                {
+                    model: Identity,
+                    attributes: ["name"],
+                },
+            ],
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
 
 export const createOrganization = async (req, res) => {
-  const {
-    name_org, image, start_year, end_year, role,
-    jobdesc
-  } = req.body;
+    try {
+        // Use the multer upload middleware to handle file uploads
+        upload.single("image")(req, res, async (err) => {
+            if (err) {
+                return res.status(500).send({
+                    message: "File cannot upload",
+                });
+            }
 
-  try {   
-    await Organization.create({
-      name_org: name_org, 
-      image: image, 
-      start_year: start_year,
-      end_year: end_year,
-      role: role,
-      jobdesc: jobdesc,
-      identityId: req.params.identityId
-    });
-    res.status(201).json({msg: "Organization Created Successfully"});
-  } catch (error) {
-    res.status(500).json({msg: error.message});
-  }
-}
+            // File upload was successful, now you can access req.file
+            const file_name = req.file.filename;
+            const { name_org, start_year, end_year, role, jobdesc } = req.body;
+            await Organization.create({
+                name_org: name_org,
+                image: file_name,
+                start_year: start_year,
+                end_year: end_year,
+                role: role,
+                jobdesc: jobdesc,
+                identityId: req.params.identityId,
+            });
+            res.status(201).json({ msg: "Organization Created Successfully" });
+        });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
 
 export const updateOrganization = async (req, res) => {
-  try {
-    const organization = await Organization.findOne({
-      where: {
-        id: req.params.organizationId
-      }
-    });
-    
-
-    if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
-
-    const {
-      name_org, image, start_year, end_year, role,
-      jobdesc
-    } = req.body;
-
-    if(req.params.identityId == organization.identityId) {
-      await Organization.update({name_org, image, start_year, end_year, 
-        role, jobdesc}, {
-          where: {
-            [Op.and] : [{id: organization.id}, {identityId: req.params.identityId }]
-          }
+    try {
+        const organization = await Organization.findOne({
+            where: {
+                id: req.params.organizationId,
+            },
         });
-    } else {
-      return res.status(403).json({msg: "Akses terlarang"});
-    } 
-    
-    res.status(200).json({msg: "Organization updated successfully"}); 
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-}
+
+        if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
+
+        const { name_org, image, start_year, end_year, role, jobdesc } = req.body;
+
+        if (req.params.identityId == organization.identityId) {
+            await Organization.update(
+                { name_org, image, start_year, end_year, role, jobdesc },
+                {
+                    where: {
+                        [Op.and]: [{ id: organization.id }, { identityId: req.params.identityId }],
+                    },
+                }
+            );
+        } else {
+            return res.status(403).json({ msg: "Akses terlarang" });
+        }
+
+        res.status(200).json({ msg: "Organization updated successfully" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
 
 export const deleteOrganization = async (req, res) => {
-  try {
-    const organization = await Organization.findOne({
-      where: {
-        id: req.params.organizationId
-      }
-    });
-    
-    if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
-
-    if(req.params.identityId == organization.identityId) {
-      await Organization.destroy({
-          where: {
-            [Op.and] : [{id: organization.id}, {identityId: req.params.identityId }]
-          }
+    try {
+        const organization = await Organization.findOne({
+            where: {
+                id: req.params.organizationId,
+            },
         });
-    } else {
-      return res.status(403).json({msg: "Akses terlarang"});
-    } 
-    
-    res.status(200).json({msg: "Organization deleted successfully"}); 
-  } catch (error) {
-    res.status(500).json({ msg: error.message });
-  }
-}
+
+        if (!organization) return res.status(404).json({ msg: "Data tidak ditemukan" });
+
+        if (req.params.identityId == organization.identityId) {
+            await Organization.destroy({
+                where: {
+                    [Op.and]: [{ id: organization.id }, { identityId: req.params.identityId }],
+                },
+            });
+        } else {
+            return res.status(403).json({ msg: "Akses terlarang" });
+        }
+
+        res.status(200).json({ msg: "Organization deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
+};
